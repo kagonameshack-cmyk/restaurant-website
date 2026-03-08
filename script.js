8e8e76405f0453450e70abc0d99dec56ea8a1608
kagonameshack-cmyk <kagonameshack@gmail.com>
2026-03-08 10:46:47 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+const sections = document.querySelectorAll("section");
+
+window.addEventListener("scroll", () => {
+sections.forEach(section => {
+const position = section.getBoundingClientRect().top;
+const screenPosition = window.innerHeight / 1.3;
+
+if(position < screenPosition){
+section.classList.add("visible");
+}
+});
+});
